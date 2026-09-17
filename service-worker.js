c86268e7fa85320f99b341a1ab3ad58883c1d7b3
pepenenedouble21-design <pepenene.double21@gmail.com>
2026-09-17 12:05:33 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='bignami-v2';const A=['./','./index.html','./farmaci.json','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
